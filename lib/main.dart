f527358337ffbfa465fdf1c9e3abd636f37b3419
kabetani-yusei <yuusei1771@gmail.com>
2024-12-08 00:05:35 +0900

    map template

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_animations/flutter_map_animations.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +15,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.amber,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'flutter_mapテスト'),
     );
   }
 }
@@ -29,40 +31,107 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  List<Marker> addMarkers = [];
+  // MapControllerのインスタンス作成
+  late final _animatedMapController = AnimatedMapController(vsync: this);
 
-  void _incrementCounter() {
+  void _addMarker(LatLng latlng) {
     setState(() {
-      _counter++;
+      addMarkers.add(
+        Marker(
+          width: 30.0,
+          height: 30.0,
+          point: latlng,
+          child: GestureDetector(
+            onTap: () {
+              _animatedMapController.animateTo(dest: latlng);
+            },
+            child: const Icon(
+              Icons.location_on,
+              color: Colors.blue,
+              size: 50,
+            ),
+          ),
+        ),
+      );
     });
+  }
+
+  void _showAlert(LatLng latlng) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('ピンの位置'),
+        content: Text('緯度: ${latlng.latitude}, 経度: ${latlng.longitude}'),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('閉じる'),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.white,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: FlutterMap(
+        // mapControllerをFlutterMapに指定
+        mapController: _animatedMapController.mapController,
+        options: MapOptions(
+          initialCenter: const LatLng(35.170915, 136.881537),
+          initialZoom: 10.0,
+          maxZoom: 12.0,
+          minZoom: 8.0,
+          onTap: (tapPosition, point) {
+            _addMarker(point);
+          },
+          // ズーム操作のイベントをキャッチ
+          onPositionChanged: (position, hasGesture) {
+            print("Zoom level: ${position.zoom}");
+          },
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          ),
+          const MarkerLayer(
+            markers: [
+              Marker(
+                width: 30.0,
+                height: 30.0,
+                point: LatLng(35.170915, 136.881537), // ピンの位置を設定
+                child: Icon(
+                  Icons.location_on,
+                  color: Colors.red,
+                  size: 50,
+                ),
+                rotate: true,
+              )
+            ],
+          ),
+          MarkerLayer(markers: addMarkers),
+          PolylineLayer(
+            polylines: [
+              Polyline(
+                points: [
+                  const LatLng(35.1, 136.85),
+                  const LatLng(35.2, 136.80),
+                  const LatLng(35.3, 136.89),
+                  const LatLng(35.4, 136.82),
+                ],
+                strokeWidth: 16.0,
+                color: Colors.black,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
